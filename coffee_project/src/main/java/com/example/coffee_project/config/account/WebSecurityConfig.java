044631bc05aaccea93b4af8a67ade714b3a09595
dinhquan2007 <dinhquan2007@gmail.com>
2023-08-06 17:12:45 +0700

    [Quan][Account] thêm phương thức lấy account

--- coffee_project/src/main/java/com/example/coffee_project/config/account/WebSecurityConfig.java
+++ coffee_project/src/main/java/com/example/coffee_project/config/account/WebSecurityConfig.java
@@ -63,6 +63,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/user/list").hasAuthority("admin")
 
         ;
+        //
+        http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/account/403");
         //    cấu hình login
         http.authorizeRequests().and()
                 .formLogin()
