70ad5c68f46ce4589d0c1f6eea34163a30d1b738
cuong19962012 <tancuong.it@gmail.com>
2023-08-07 17:26:58 +0700

    [coffee][Cuong][Order]Update base future order 3

--- coffee_project/src/main/java/com/example/coffee_project/repository/oder/IOrderRepository.java
+++ coffee_project/src/main/java/com/example/coffee_project/repository/oder/IOrderRepository.java
@@ -1,10 +1,11 @@
 package com.example.coffee_project.repository.oder;
 
 import com.example.coffee_project.model.oder.Order;
+import com.example.coffee_project.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IOrderRepository extends JpaRepository<Order, Integer> {
     Order findByOrderId(int orderId);
 
-    Order findByOrderStatus(boolean status);
+    Order findByOrderStatusAndAndUser(boolean status, User user);
 }
