f0c4dc65a1c05241943677f12322da529d4336de
hoangnhat3103@gmail.com <hoangnhat3103@gmail.com>
2023-08-07 18:05:12 +0700

    [COFFEE][NHAT][USER]updated delete, view

--- coffee_project/src/main/java/com/example/coffee_project/repository/user/IUserRepository.java
+++ coffee_project/src/main/java/com/example/coffee_project/repository/user/IUserRepository.java
@@ -4,12 +4,25 @@ import com.example.coffee_project.model.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 
-public interface    IUserRepository extends JpaRepository<User,Integer> {
+public interface IUserRepository extends JpaRepository<User, Integer> {
     Page<User> findUserByUserNameContaining(Pageable pageable, String search);
+
     User findUserByAccountAccountName(String name);
+
     User findUserByUserPhoneNumber(String numberPhone);
+
     User findUserByUserEmail(String email);
+
     User findUserByUserIdCard(String idCard);
+
+    @Transactional
+    @Modifying
+    @Query(value = "call remove_user(:id) ;",nativeQuery = true)
+    void removeUserByUserId(@Param("id") Integer id);
 }
