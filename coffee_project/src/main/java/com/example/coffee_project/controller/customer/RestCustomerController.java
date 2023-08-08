539eed700ba43be380ff547f224173b62c8976c6
htquyen53 <quinnquinnisme@gmail.com>
2023-08-08 10:51:01 +0700

    [QUYEN][CUSTOMER] modal

---
+++ coffee_project/src/main/java/com/example/coffee_project/controller/customer/RestCustomerController.java
@@ -0,0 +1,25 @@
+package com.example.coffee_project.controller.customer;
+
+import com.example.coffee_project.model.customer.Customer;
+import com.example.coffee_project.service.customer.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/customer")
+public class RestCustomerController {
+    @Autowired
+    private ICustomerService customerService;
+
+    @GetMapping("/view/{id}")
+    public ResponseEntity<Customer> showDetail(@PathVariable Integer id) {
+        Customer customer = customerService.findById(id);
+        if (customer != null) {
+            return new ResponseEntity<>(customer, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
