65763aeedfa379b777c7a70663f5b64f6cac408f
dinhquan2007 <dinhquan2007@gmail.com>
2023-08-09 14:58:58 +0700

    [Quan][Account] sửa lại thêm mới account và không hiển thị account admin

--- coffee_project/src/main/java/com/example/coffee_project/service/account/IAccountService.java
+++ coffee_project/src/main/java/com/example/coffee_project/service/account/IAccountService.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface IAccountService extends UserDetailsService {
-    Page<Account> getAllAccount(Pageable pageable, String searchName);
+    Page<Account> getAllAccount(Pageable pageable, String searchName,String roleName);
     void save(AccountDto accountDto);
     void forgot(Account account);
     Account findByUsername(String username);
@@ -15,4 +15,7 @@ public interface IAccountService extends UserDetailsService {
      String sendEmailAndReturnCode(String to);
 
     void deleteAccount(Account account);
+    boolean testPass(String username,String pass);
+
+    void changePass(String username,String newPassword);
 }
